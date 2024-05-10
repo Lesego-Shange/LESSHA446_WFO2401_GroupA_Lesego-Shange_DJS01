398d20dd293aa410411386d49acbdf8413b334c0
Lesego Shange <makgeledisal@gmail.com>
2024-05-10 10:57:59 +0200

    Corrected calculations

--- index.js
+++ index.js
@@ -13,14 +13,14 @@ const initialDistance = 0; // Initial distance in km
 const initialFuel = 5000; // Initial fuel amount in kg
 const fuelBurnRate = 0.5; // Fuel burn rate in kg/s
 
-const newDistance = initialDistance + initialVelocity * duration; //calcultes new distance
-const remainingFuel = fuelBurnRate * duration; //calculates remaining fuel
+const newDistance = initialDistance + initialVelocity * (duration / 3600); //calcultes new distance
+const const remainingFuel = fuelBurnRate * (duration / 3600); //calculates remaining fuel
 const newVelocity = calcNewVel(acceleration, initialVelocity, duration); //calculates new velocity based on acceleration
 
 // Pick up an error with how the function below is called and make it robust to such errors
 calcNewVel = (initialVelocity, acceleration, duration) => {
-  return initialVelocity + acceleration * duration;
-};
+  return initialVelocity + (acceleration * (duration / 3600));
+}
 
 console.log(`Corrected New Velocity: ${newVelocity} km/h`);
 console.log(`Corrected New Distance: ${newDistance} km`);
